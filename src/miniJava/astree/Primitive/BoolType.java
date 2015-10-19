0ca09e10eb819577eab8361b89267ea58a3304bb
leoschet <leoschettini2@gmail.com>
2015-10-19 00:27:28 -0200

    add primitive types and updates Lexer.java

---
+++ src/miniJava/astree/Primitive/BoolType.java
@@ -0,0 +1,9 @@
+package miniJava.astree.primitive;
+import miniJava.astree.INode;
+
+public class BoolType implements INode {
+
+    public BoolType() {
+
+    }
+}
---
+++ src/miniJava/astree/Primitive/IntType.java
@@ -0,0 +1,12 @@
+package miniJava.astree.primitive;
+import miniJava.astree.INode;
+
+/**
+ * Created by Leonardo on 18/10/2015.
+ */
+public class IntType implements INode {
+
+    public IntType() {
+
+    }
+}
